1cf0c251f131739d7ca38354536d6c9e3511f5ee
abdulaziz-m5u <azizabdulaziz70932@gmail.com>
2022-03-20 15:25:15 +0800

    update readme & route

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('auth.login');
 });
 
 Route::group(['middleware' => 'auth','prefix' => 'admin', 'as' => 'admin.'], function() {
@@ -36,6 +36,3 @@ Route::group(['middleware' => 'auth','prefix' => 'admin', 'as' => 'admin.'], fun
 
 Auth::routes();
 
-Route::get('/home', function() {
-    return view('home');
-})->name('home');
